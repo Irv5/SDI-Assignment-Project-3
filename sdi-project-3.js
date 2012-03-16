5e826fe74bf4ed2be80800ae3c276147e7aaa567
Irvin Reaves <irv5@Irvins-MacBook-Pro.local>
2012-03-15 20:57:36 -0400

    Updated .thml file and created Variables in my .js file

---
+++ sdi-project-3.js
@@ -0,0 +1,12 @@
+// Project 3
+// Author: Irvin Reaves
+// Created for: SDI Online
+
+
+var player = {			// properties
+	name: "David Smith",
+	country: "USA",
+	totalScore: -17,
+	winner: true,
+	roundsTotal: [64, 68, 69, 70], 
+	teeOff: " at 8:25 AM",
